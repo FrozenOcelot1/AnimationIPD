89cf3eafca3884403e4cdd7cdb08b334a68da98a
Luke <107695876+FrozenOcelot1@users.noreply.github.com>
2022-06-17 17:41:16 -0300

    Update scene.py
    new better code

--- scene.py
+++ scene.py
@@ -1,42 +1,83 @@
 from manim import *
+import math
+import numpy as np
 
 class CreateAnimation(Scene):
     def construct(self):
-        scale=0.5
-        shiftx=-1
-        shifty=1.5
-        # Utility function for coordinate transformation
-        def xy(a,b):
-            return [a*scale+shiftx,b*scale+shifty,0]
-         # Draw equation
-        equation = MathTex(
-            "a^2 = b^2 + c^2", color=YELLOW, font_size=96
+        
+        tex = Tex('Teorema de Pitagoras', font_size=34)
+        tex.move_to(3*UP)
+
+        pythag = MathTex(
+            "a^2 = b^2 + c^2"
         )
-        equation2 = MathTex (
-            "a^2 = b^2 + c^2", color=YELLOW, font_size=50
-        )
-        self.add(equation)
-        self.wait(0.5)
-        #self.play(FadeOut(equation))
-        triangle = Polygon(xy(-3,-2),xy(3,-2),xy(-3,2),fill_color=RED, fill_opacity=0.9, color=WHITE)
-        square1 = Square(2).set_fill(BLUE, opacity=0.6)
-        square2 = Square(3).set_fill(BLUE, opacity=0.6)
-        square1.next_to(triangle, LEFT)
-        square2.next_to(triangle, DOWN)
-        hypotenuse = Square(3.6).set_fill(BLUE, opacity=0.6)
-        self.play(Transform(equation,equation2))
-        self.play(equation.animate.shift(DOWN))
-        self.wait(0.5)
-        self.play(DrawBorderThenFill(triangle), run_time=0.75)
-        self.play(FadeOut(equation))
-        self.play(triangle.animate.shift(DOWN * 1), run_time=0.9)
-        self.play(Create(square1))
-        self.play(Create(square2))
-        self.wait(0.5)
-        self.play(FadeOut(square1))
-        self.play(FadeOut(square2))
-        #self.play(triangle.animate.shift(DOWN * 1), run_time=0.9)
-        self.play(Create(hypotenuse))
-        #self.remove(triangle)
-        self.wait(3)
+        pythag.move_to(3*RIGHT + 1.3*UP)
+        pythag.scale(0.85)
+        pythag[0][0].set_color(BLUE)
+        pythag[0][3].set_color(GREEN)
+        pythag[0][6].set_color(RED)
+
+           
+        START_1 = (-1.5,-1.5,0)
+        END_1 =   (-1.5,1.5,0)
+
+        START_2 = (-1.5,1.5,0)
+        END_2 =   (1.5,-1.5,0)
+
+        START_3 = (1.5,-1.5,0)
+        END_3 =   (-1.5025,-1.5,0)
+
+        START_4 = (-1.2,-1.5,0)
+        END_4 =   (-1.2,-1.2,0)
+
+        START_5 = (-1.2,-1.2,0)
+        END_5 =   (-1.5025,-1.2,0)
+
+
+        line_1 = Line(START_1,END_1)
+        line_2 = Line(START_2,END_2)
+        line_3 = Line(START_3,END_3)
+        line_4 = Line(START_4,END_4)
+        line_5 = Line(START_5,END_5)
+
+        x = Tex("a")
+        x.set_color_by_tex("a", BLUE)
+        x.next_to(line_1, LEFT)
+        x.scale(0.75)
+
+        y = Tex("b")
+        y.set_color_by_tex("b", GREEN)
+        y.next_to(line_3, DOWN)
+        y.scale(0.75)
+
+        z = Tex("c")
+        z.set_color_by_tex("c", RED)
+        z.move_to(0.4*RIGHT)
+        z.scale(0.75)
+
+        self.play(Write(tex))
+        self.wait(2)
+
+        self.play(Create(line_1))
+        self.play(Create(line_2))
+        self.play(Create(line_3))
+        self.wait(1.5)
+        self.play(Create(line_4))
+        self.play(Create(line_5))
+
+
+        self.play(Write(x))
+        self.play(Write(y))
+        self.play(Write(z))
+        self.wait(1.5)
+
+        self.play(Write(pythag), run_time = 2)
+
+        # self.play(ReplacementTransform(x.copy(), pythag[0]))
+        # self.play(ReplacementTransform(y.copy(), pythag[3]))
+        # self.play(ReplacementTransform(z.copy(), pythag[6]))
+        self.wait(2)
+
+        self.play(FadeOut(line_1), FadeOut(line_2),  FadeOut(line_3), FadeOut(line_4), 
+        FadeOut(line_5), FadeOut(x), FadeOut(y), FadeOut(z), FadeOut(tex),FadeOut(pythag))
 
